f9acb7aed9a34b9f7b7af60dd592d21dd74c8b79
Kenny Pflug <kenny.pflug@live.de>
2017-09-17 10:18:27 +0200

    added example for custom lifetimes

---
+++ Code/BestPractices/CustomLifetimes.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using FluentAssertions;
+using LightInject;
+using Xunit;
+
+namespace BestPractices
+{
+    public sealed class CustomLifetimes
+    {
+        [Fact]
+        public void CustomPerThreadLifetime()
+        {
+            var container = new ServiceContainer();
+            container.Register<ConcurrentSpy>(new PerThreadLifetime());
+            void ResolveD() => container.GetInstance<ConcurrentSpy>();
+            var thread1 = new Thread(ResolveD);
+            var thread2 = new Thread(ResolveD);
+
+            thread1.Start();
+            thread2.Start();
+            thread1.Join();
+            thread2.Join();
+
+            ConcurrentSpy.NumberOfInstances.Should().Be(2);
+        }
+
+        public sealed class ConcurrentSpy
+        {
+            private static int _numberOfInstances;
+
+            public static int NumberOfInstances => Volatile.Read(ref _numberOfInstances);
+
+            public ConcurrentSpy()
+            {
+                Interlocked.Increment(ref _numberOfInstances);
+            }
+        }
+
+        public sealed class PerThreadLifetime : ILifetime
+        {
+            private readonly ThreadLocal<object> _threadLocal = new ThreadLocal<object>();
+
+            public object GetInstance(Func<object> createInstance, Scope scope)
+            {
+                if (_threadLocal.IsValueCreated == false)
+                    _threadLocal.Value = createInstance();
+
+                return _threadLocal.Value;
+            }
+        }
+    }
+}
